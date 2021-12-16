a49307d884069877a940ff8a1103fd6f53341a77
Sonu Paswan <88018101+sonustein@users.noreply.github.com>
2021-12-16 17:36:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-// for dice one
+function startGame() {
+  // for dice one
+  let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  let imageRandom1 = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
+  let image1 = document.querySelectorAll("img")[0]; //selecting attribute by tag name
+  image1.setAttribute("src", imageRandom1); // changing attribute
+  // note imageRandom is already a string
 
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let imageRandom1 = "images/dice" + randomNumber1 + ".png"; //images/dice1.png - images/dice6.png
-let image1 = document.querySelectorAll("img")[0]; //selecting attribute by tag name
-image1.setAttribute("src", imageRandom1); // changing attribute
-// note imageRandom is already a string
+  // for dice two
+  let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  let imageRandom2 = "images/dice" + randomNumber2 + ".png";
+  let image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", imageRandom2);
 
-// for dice two
+  // conditions to win game
 
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
-let imageRandom2 = "images/dice" + randomNumber2 + ".png";
-let image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", imageRandom2);
-
-// conditions to win game
-
-// selecting h1 tag
-let heading = document.querySelector("h1");
-// changing h1 according to conditions
-if (randomNumber1 > randomNumber2) {
-  heading.innerHTML = "Jack Wins";
-} else if (randomNumber1 < randomNumber2) {
-  heading.innerHTML = "Rose Wins";
-} else {
-  heading.innerHTML = "Draw";
+  // selecting h1 tag
+  let heading = document.querySelector("h1");
+  // changing h1 according to conditions
+  if (randomNumber1 > randomNumber2) {
+    heading.innerHTML = "Jack Wins";
+  } else if (randomNumber1 < randomNumber2) {
+    heading.innerHTML = "Rose Wins";
+  } else {
+    heading.innerHTML = "Draw";
+  }
 }
